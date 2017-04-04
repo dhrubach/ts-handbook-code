9e67943a03cbb60b47d4547bea6b7dc509b1118a
Dhruba Chatterjee <dhrubach@hotmail.com>
2017-04-04 07:16:03 +0530

    feat(types) : function and advanced types

---
+++ src/types-interfaces/05-function-types.ts
@@ -0,0 +1,18 @@
+namespace interface.functionTypes {
+
+	console.log(`****************example 01***************`);
+	console.log(``);
+
+	interface ICheckFunc {
+		(input: string, check: string, position: number): boolean;
+	}
+
+	let stringSearch: ICheckFunc;
+	// type of parameters in corresponding positions are validated, not parameter names
+	stringSearch = function(src: string, chk: string, startPos: number) {
+		return src.indexOf(chk, startPos) > -1;
+	};
+
+	console.log(`'type' in 'typescript' : ${stringSearch('typescript', 'type', 0)}`);
+	console.log(`'java' in 'typescript' : ${stringSearch('typescript', 'java', 0)}`);
+}
