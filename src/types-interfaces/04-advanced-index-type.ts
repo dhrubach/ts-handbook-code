890004458ae6b14fcbcc2e09b1fc9ed8e775dbef
Dhruba Chatterjee <dhrubach@hotmail.com>
2017-04-10 00:10:35 +0530

    update(types) : intersection types

--- src/types-interfaces/04-advanced-index-type.ts
+++ src/types-interfaces/04-advanced-index-type.ts
@@ -1,22 +1,15 @@
-namespace advancedTypes.indexTypes {
+/*
+*	index types : check for dynamic property names
+*		- index type query operator : keyof
+*		- indexed access operator : T[k]
+*/
+console.log(`****************example 01***************`);
+console.log(``);
 
-	/*
-	*	index types : check for dynamic property names
-	*		- index type query operator : keyof
-	*		- indexed access operator : T[k]
-	*/
-
-
-	console.log(`****************example 01***************`);
-	console.log(``);
-
-	/*
-	*	'keyof T' : for a given type 'T', 'keyof T' will return an union of
-	*	known, public property names of 'T'
-	*/
-	function pluck<T, K extends keyof T>(object: T, propNames: K[]): T[K][] {
-		return propNames.map(prop => object[prop]);
-	}
-
-
+/*
+*	'keyof T' : for a given type 'T', 'keyof T' will return an union of
+*	known, public property names of 'T'
+*/
+function pluck<T, K extends keyof T>(object: T, propNames: K[]): T[K][] {
+	return propNames.map(prop => object[prop]);
 }
---
+++ src/types-interfaces/06-advanced-intersection-type.ts
@@ -0,0 +1,52 @@
+console.log(`****************example 01***************`);
+
+class Proposal {
+	constructor(
+		public name: string,
+		public id: number,
+		public amount: number) { }
+}
+
+class Owner {
+	constructor(
+		public name: string,
+		public dob: string
+	) { }
+}
+
+function combine<T, U>(first: T, second: U): T & U {
+	let result = <T & U>{};
+
+	for (let prop in first) {
+		(<any>result)[prop] = (<any>first)[prop];
+	}
+
+	for (let prop in second) {
+		if (!result.hasOwnProperty(prop)) {
+			(<any>result)[prop] = (<any>second)[prop];
+		}
+	}
+
+	return result;
+}
+
+function displayDetails(combinedObject: Proposal & Owner): void {
+	console.log(`
+		Type of combined type : ${typeof combinedObject}
+		Name of the proposal : ${combinedObject.name}
+		Date of Birth : ${combinedObject.dob}
+	`);
+}
+
+const proposal = new Proposal("prop-test-intersection", 12345, 900000);
+const account = new Owner("John", "01/01/1970");
+
+let combinedType = combine(proposal, account);
+displayDetails(combinedType);
+
+// reversing the order of the combined types
+combinedType = combine(account, proposal);
+// note the value of combinedType.name
+displayDetails(combinedType);
+
+export { Proposal, Owner };
